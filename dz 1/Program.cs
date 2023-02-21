66e90eae5b8e92c3cce0526366a582a63026afed
Elya <mirademina22@gmail.com>
2023-02-22 00:27:28 +0300

    otvet dz1 new

--- dz 1/Program.cs
+++ dz 1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1.Найти кубы чисел от 1 до N
+Console.WriteLine ("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+
+{
+    Console.WriteLine($"{i} \t {i*i*i}" );
+}

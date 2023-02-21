bc5178a6e14453d74a16f36781a3771033cb1e30
Elya <mirademina22@gmail.com>
2023-02-22 00:25:49 +0300

    otvet dz3

---
+++ dz 3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число1: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число2: ");
+int number2 = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+
+
+{
+    Console.WriteLine( "Произведение чисел" {number1 * number2} );
+}
